4728022acad9aadfb35d6799a53c3658354bf920
Brando <bbaker.sh@gmail.com>
2024-03-26 15:18:28 -0700

    damn i forgot where i stopped lol

--- cmd/main.go
+++ cmd/main.go
@@ -12,6 +12,64 @@ type Templates struct {
 	templates *template.Template
 }
 
+type Contact struct {
+	Name  string
+	Email string
+}
+
+type Contacts = []Contact
+
+type Data struct {
+	Contacts Contacts
+}
+
+type FormData struct {
+	Values map[string]string
+	Errors map[string]string
+}
+
+type Page struct {
+	Data Data
+	Form FormData
+}
+
+func newPage() Page {
+	return Page{
+		Data: newData(),
+		Form: newFormData(),
+	}
+}
+
+func newFormData() FormData {
+	return FormData{
+		Values: make(map[string]string),
+		Errors: make(map[string]string),
+	}
+}
+
+func newContact(name, email string) Contact {
+	return Contact{
+		Name:  name,
+		Email: email,
+	}
+}
+func (d Data) hasEmail(email string) bool {
+	for _, contact := range d.Contacts {
+		if contact.Email == email {
+			return true
+		}
+	}
+	return false
+}
+func newData() Data {
+	return Data{
+		Contacts: []Contact{
+			newContact("Brandon", "bb@gmail.com"),
+			newContact("Kieluna", "kh@gmail.com"),
+		},
+	}
+}
+
 func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
 	return t.templates.ExecuteTemplate(w, name, data)
 }
@@ -22,24 +80,33 @@ func NewTemplates() *Templates {
 	}
 }
 
-type Count struct {
-	Count int
-}
-
 func main() {
 	e := echo.New()
 	e.Use(middleware.Logger())
 
-	count := Count{Count: 0}
+	page := newPage()
 	e.Renderer = NewTemplates()
 
 	e.GET("/", func(c echo.Context) error {
-		return c.Render(200, "index", count)
+		return c.Render(200, "index", page)
 	})
 
-	e.POST("/count", func(c echo.Context) error {
-		count.Count++
-		return c.Render(200, "count", count)
+	e.POST("/contacts", func(c echo.Context) error {
+		name := c.FormValue("name")
+		email := c.FormValue("email")
+
+		if page.Data.hasEmail(email) {
+			FormData := newFormData()
+			FormData.Values["name"] = name
+			FormData.Values["email"] = email
+			FormData.Errors["email"] = "Email already exists"
+			return c.Render(422, "form", FormData)
+		}
+
+		contact := newContact(name, email)
+		page.Data.Contacts = append(page.Data.Contacts, contact)
+		c.Render(200, "form", newFormData())
+		return c.Render(200, "oob-contact", contact)
 	})
 	e.Logger.Fatal(e.Start(":42069"))
 }
